68d3ca1714bad34b51f1e8a85428f4bd865a2b5d
MaruVadala89 <maruvad15@gmail.com>
2023-10-25 21:43:09 -0300

    cambios

---
+++ preentrega1vadala/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const CartWidget = () => {
+    return (
+        <>
+            <img src="" alt="" />
+            <p>10</p>
+            
+        </>
+    );
+};
+
+export default CartWidget;
---
+++ preentrega1vadala/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./ItemListContainer.css"
+
+
+const ItemListContainer = ({mensaje}) => {
+
+    return (
+        <>
+            <h1>{mensaje}</h1>
+        </>
+    );
+};
+
+
+export default ItemListContainer;
