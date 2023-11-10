4622f20fe3d60cc8ef8b4c40c4ba2fadc59fdf57
MaruVadala89 <maruvad15@gmail.com>
2023-11-10 13:33:20 -0300

    cambio

--- preentrega1vadala/src/App.jsx
+++ preentrega1vadala/src/App.jsx
@@ -8,6 +8,7 @@ import NavBar from './components/NavBar/NavBar'
 import Ubicacion from './components/Ubicacion/Ubicacion'
 import Footer from './components/Footer/Footer'
 import Contacto from './components/Contacto/Contacto'
+import Error from './components/Error/Error'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 
@@ -24,10 +25,11 @@ function App() {
     <BrowserRouter>
     <Routes>
 
-      <Route path='/' element={<ItemListContainer/>}/> /*siempre el primer path es '/' */ 
+      <Route path='/' element={<ItemListContainer/>}/> {/*siempre el primer path es '/' */}
       <Route path='/detalle' element={<ItemDetailContainer/>}/>
       <Route path='/contacto' element={<Contacto/>}/>
-     
+      <Route path='/ubicacion' element={<Ubicacion/>}/>
+      <Route path='*' element={<Error/>}/>
 
 
     </Routes>   
