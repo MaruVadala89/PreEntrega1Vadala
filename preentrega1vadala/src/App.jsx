43e3b02760261767b52323a7ba1cbf245577b816
MaruVadala89 <maruvad15@gmail.com>
2023-11-10 13:23:14 -0300

    cambio

--- preentrega1vadala/src/App.jsx
+++ preentrega1vadala/src/App.jsx
@@ -1,9 +1,17 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
+import NavBar from './components/NavBar/NavBar'
+import Ubicacion from './components/Ubicacion/Ubicacion'
+import Footer from './components/Footer/Footer'
+import Contacto from './components/Contacto/Contacto'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+
+
 
 
 
@@ -13,9 +21,23 @@ function App() {
   return (
     <>
 
-    <NavBar/>
+    <BrowserRouter>
+    <Routes>
+
+      <Route path='/' element={<ItemListContainer/>}/> /*siempre el primer path es '/' */ 
+      <Route path='/detalle' element={<ItemDetailContainer/>}/>
+      <Route path='/contacto' element={<Contacto/>}/>
+     
+
+
+    </Routes>   
+    
+    </BrowserRouter>
+
+
     <ItemListContainer/>
-    <ItemDetailContainer productId={1}/>
+    <ItemDetailContainer productId={2}/>
+
     
     </>
 
--- preentrega1vadala/src/components/NavBar/NavBar.jsx
+++ preentrega1vadala/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import "./NavBar.css"
-import CartWidget from '../CartWidget/CartWidget';
+
 
 const NavBar = () => {
     return (
-      <nav>
-        <ul>
-            <h1 className="titulo">Adopta una planta</h1>
-            <li><a href="#">Inicio</a></li>
-            <li> <a href="#">Plantas</a></li>
-            <li> <a href="#">Contacto</a></li>
-        </ul>
-        <CartWidget/>
-      </nav>
+      <>
+      <div>
+
+      <h1>NavBar</h1>
+
+      </div>
+        
+      </>
     );
 };
 
