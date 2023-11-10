ce79a82744e274012fa223d61390f160f8035ff6
MaruVadala89 <maruvad15@gmail.com>
2023-11-10 11:40:50 -0300

    router

--- preentrega1vadala/src/App.jsx
+++ preentrega1vadala/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import ItemCount from './components/ItemCount/ItemCount'
+
 
 
 function App() {
